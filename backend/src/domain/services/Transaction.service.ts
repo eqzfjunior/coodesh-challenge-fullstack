81848dba993508a8e245c08347ddb662b049dd96
Edinaldo Júnior <eqzfjunior@gmail.com>
2023-09-11 19:11:42 -0300

    Remove implementation summary

--- backend/src/domain/services/Transaction.service.ts
+++ backend/src/domain/services/Transaction.service.ts
@@ -3,7 +3,6 @@ import {
   Transaction,
   TransactionType,
 } from '../interfaces/Transaction.interface';
-import { SummaryTransaction } from '../interfaces/SummaryTransaction.interface';
 import { ISellerRepository } from '../repositories/Seller.repository';
 import { SellerEntity } from '../entities/Seller.entity';
 import { IProductRepository } from '../repositories/Product.repository';
@@ -29,60 +28,7 @@ export class TransactionService {
     private commissionRepository: ICommissionRepository,
   ) {}
 
-  summary(transactions: Transaction[]): SummaryTransaction[] {
-    const summary: SummaryTransaction[] = [];
-
-    transactions.forEach((transaction) => {
-      const index = summary.findIndex(
-        (item) => item.sellerName == transaction.sellerName,
-      );
-      let item: SummaryTransaction;
-
-      if (index == -1) {
-        item = {
-          sellerName: transaction.sellerName,
-          totalSales: 0,
-          totalCommissionsPaid: 0,
-          totalCommissionsReceived: 0,
-          balance: 0,
-        } as SummaryTransaction;
-      } else {
-        item = summary[index];
-      }
-
-      if (
-        [
-          TransactionType.SALE_PRODUCER,
-          TransactionType.SALE_AFFILIATE,
-        ].includes(transaction.type)
-      ) {
-        item.totalSales = transaction.value + item.totalSales;
-      }
-
-      if ([TransactionType.COMMISSION_RECEIVED].includes(transaction.type)) {
-        item.totalCommissionsReceived =
-          transaction.value + item.totalCommissionsReceived;
-      }
-
-      if ([TransactionType.COMMISSION_PAID].includes(transaction.type)) {
-        item.totalCommissionsPaid =
-          transaction.value + item.totalCommissionsPaid;
-      }
-
-      item.balance =
-        item.totalSales -
-        item.totalCommissionsPaid +
-        item.totalCommissionsReceived;
-
-      if (index == -1) {
-        summary.push(item);
-      } else {
-        summary[index] = item;
-      }
-    });
-
-    return summary;
-  }
+  summary(): void {}
 
   async import(transactions: Transaction[]): Promise<void> {
     for await (const transaction of transactions) {
