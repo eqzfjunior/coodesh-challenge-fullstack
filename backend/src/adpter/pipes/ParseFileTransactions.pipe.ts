487b2cc6210e0e7b2a730c7d03a9d4407b2f9b25
Edinaldo Júnior <eqzfjunior@gmail.com>
2023-09-11 19:13:28 -0300

    Pipe parse of content file upload

---
+++ backend/src/adpter/pipes/ParseFileTransactions.pipe.ts
@@ -0,0 +1,26 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { Transaction } from '../../domain/interfaces/Transaction.interface';
+
+@Injectable()
+export class ParseTransactionsPipe implements PipeTransform {
+  transform(
+    file: Express.Multer.File,
+    metadata: ArgumentMetadata,
+  ): Transaction[] {
+    const content = file.buffer.toString('utf-8');
+    const lines = content
+      .split('\n')
+      .filter((line) => line)
+      .map((line) => {
+        return {
+          type: parseInt(line.slice(0, 1)),
+          date: new Date(line.slice(1, 26)),
+          productName: line.slice(26, 56).trim(),
+          value: Number(line.slice(56, 66)),
+          sellerName: line.slice(66, 86),
+        };
+      });
+
+    return lines;
+  }
+}
