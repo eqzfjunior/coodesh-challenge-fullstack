c7ac5fd3b758835c7e57475f03e59bd46d1a6903
Edinaldo Júnior <eqzfjunior@gmail.com>
2023-09-11 19:14:20 -0300

    Controller method upload

---
+++ backend/src/adpter/controllers/transation.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ParseTransactionsPipe } from '../pipes/ParseFileTransactions.pipe';
+import { Transaction } from '../../domain/interfaces/Transaction.interface';
+import { TransactionService } from '../../domain/services/Transaction.service';
+
+@Controller('Transactions')
+export class TransactionsController {
+  constructor(private transactinosService: TransactionService) {}
+
+  @Post('import')
+  @UseInterceptors(FileInterceptor('file'))
+  async import(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'text',
+        })
+        .addMaxSizeValidator({
+          maxSize: 10240,
+        })
+        .build(),
+      ParseTransactionsPipe,
+    )
+    transactions: Transaction[],
+  ): Promise<void> {
+    await this.transactinosService.import(transactions);
+  }
+}
